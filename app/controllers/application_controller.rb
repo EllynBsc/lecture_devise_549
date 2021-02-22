a78b7c4bd7d75cd088607a17912734d5a61f0066
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2021-02-22 10:04:46 +0100

    devise lecture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # before running any action from this controller, execute this authenticate user method
+
+  # method is used if you want to add some more columns to your user model on sign up or on login.
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar, :first_name, :last_name])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+ #Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+#   database_authenticatable: store email + encrypted password in DB
+#   registerable: register, destroy profile and login
+#   recoverable: provides mechanism to reset forgotten password
+#   rememberable: cookies db in the browser for your email and password
+#   validatable: validates email and password
+
+# confirmable: users need to confirm their mails
+#  lockable: too many login attempts
+#  timeoutable: certain time for the login
+ # trackable: to track last sign in last sign out
+ # omniauthable : sign up with a third party, fb, google, twitter, gmail,github (+config)
+end
